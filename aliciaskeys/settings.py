9b519dafc40d08d254a96b22a361a47cb68f0dd4
Becky Smith <rebkwok@gmail.com>
2018-04-22 22:45:51 +0100

    Add cookie policy and banner

--- aliciaskeys/settings.py
+++ aliciaskeys/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'cookielaw',
     'django_extensions',
     'debug_toolbar',
     'website'
